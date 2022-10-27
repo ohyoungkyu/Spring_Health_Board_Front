e279600a21320de5867f180c3cc3b03d85f2e82f
ohyoungkyu <come8908@gmail.com>
2022-10-27 13:42:43 +0900

    main page ui 수정

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,42 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Style from "../Style.css";
 const Navbar = () => {
   return (
     <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://www.w3schools.com/w3css/4/w3.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Montserrat"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
+      </Helmet>
       <div className="header">
-        <div className="logotype">NewsApp</div>
-        <div className="search">
-          <input type="search" placeholder="검색어를 입력하세요." />
-        </div>
-        <div className="nav-icon">
-          <span style={{ marginLeft: "10px", marginRight: "10px" }}>
-            로그인
-          </span>
-          <span style={{ marginLeft: "10px", marginRight: "10px" }}>
-            회원가입
-          </span>
+        <div className="logotype">Health</div>
+        <div className="top-bar">
+          <div className="search">
+            <input type="search" placeholder="검색어를 입력하세요." />
+          </div>
+
+          <div className="top-bar-right">
+            <span style={{ marginLeft: "10px", marginRight: "20px" }}>
+              로그인
+            </span>
+            <span style={{ marginLeft: "10px", marginRight: "20px" }}>
+              회원가입
+            </span>
+          </div>
         </div>
       </div>
     </>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-import Style from "../Style.css";
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
-  return <div className="side-bar"></div>;
+  return (
+    <>
+      <div className="side-bar">
+        <div>
+          <Link className="side-menu" to="/">
+            가슴
+          </Link>
+          <Link className="side-menu" to="/">
+            등
+          </Link>
+          <Link className="side-menu" to="/">
+            어깨
+          </Link>
+          <Link className="side-menu" to="/">
+            팔
+          </Link>
+          <Link className="side-menu" to="/">
+            허벅지
+          </Link>
+          <Link className="side-menu" to="/">
+            햄스트링
+          </Link>
+          <Link className="side-menu" to="/">
+            종아리
+          </Link>
+          <Link className="side-menu" to="/">
+            복근
+          </Link>
+        </div>
+      </div>
+      <div className="main"></div>
+    </>
+  );
 };
 
 export default Sidebar;
